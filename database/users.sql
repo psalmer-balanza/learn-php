e1de7ca726c902f8b33bba3d62bb122e7a891d5c
Psalmer TMDD <psalmerbalanza@gmail.com>
2025-01-31 00:54:28 -0800

    Uploaded database und favicon

---
+++ database/users.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 31, 2025 at 08:54 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `learn_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `role`) VALUES
+('mrguy', '123', 'His role is to learn how php works'),
+('second_user', 'XD', 'XD'),
+('teacher_user', 'password', 'This is a teacher');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $result = $stmt->get_result();
 $ctr = 0;
 while ($row = $result->fetch_assoc()) {
     $ctr++;
-    echo "Username " . $ctr . ":" . $row["username"] . "<br>";
+    echo "User " . $ctr . ": " . $row["username"] . "<br>";
     echo "Password " . $ctr . ":" . $row["password"] . "<br>";
     echo "Role " . $ctr . ":" . $row["role"] . "<br>";
     echo "<hr>";    
